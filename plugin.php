3cb8449cf9f1246eab75b79075054d35fc362e39
Ari Stathopoulos <aristath@gmail.com>
2021-06-25 14:40:29 +0300

    Add basic files

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Updater tweaks POC
+ * 
+ * This plugin aims to address the following:
+ *      - Plugin & Theme Rollbacks, safe-unzip
+ *      - DB upgrades & downgrades
+ *      - Plugin Dependencies
+ */
