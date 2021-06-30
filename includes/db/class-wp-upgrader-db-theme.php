8dbcd6f664d7cf4f661539db6acc2ddfda551372
Ari Stathopoulos <aristath@gmail.com>
2021-06-30 10:40:15 +0300

    run migrations for themes

--- includes/db/class-wp-upgrader-db-theme.php
+++ includes/db/class-wp-upgrader-db-theme.php
@@ -48,6 +48,25 @@ class WP_Upgrader_DB_Theme extends WP_Upgrader_DB {
 	 * @return bool|WP_Error The passed in $return param or WP_Error.
 	 */
 	public function maybe_run_migrations( $response, $hook_extra, $result ) {
+		if ( isset( $hook_extra['theme'] ) ) {
+			$old_version = $this->current_version;
+			$new_version = get_file_data(
+				$result['destination'] . 'style.css',
+				array( 'Version' => 'Version' )
+			)['Version'];
+
+			if (
+				$response && // The response is not an error.
+				$this->name === $hook_extra['theme'] && // We're updating the right thing.
+				! empty( $new_version ) && // The new version exists.
+				! empty( $old_version ) && // The old version exists.
+				$new_version !== $old_version // The new version is not the same as the old version.
+			) {
+				$this->run_migrations( $old_version, $new_version );
+			}
+
+			$this->set_version( $new_version );
+		}
 		return $response;
 	}
 }
