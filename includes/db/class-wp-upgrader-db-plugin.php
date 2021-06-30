2874ae4e4064db41f71e8a73e9d1ea687582b036
Ari Stathopoulos <aristath@gmail.com>
2021-06-30 10:40:01 +0300

    fix plugin warnings when updating theme

--- includes/db/class-wp-upgrader-db-plugin.php
+++ includes/db/class-wp-upgrader-db-plugin.php
@@ -55,23 +55,23 @@ class WP_Upgrader_DB_Plugin extends WP_Upgrader_DB {
 	 * @return bool|WP_Error The passed in $return param or WP_Error.
 	 */
 	public function maybe_run_migrations( $response, $hook_extra, $result ) {
-		$plugin      = get_plugin_data( $result['local_destination'] . '/' . $hook_extra['plugin'] );
-		$old_version = $this->current_version;
-		$new_version = $plugin['Version'];
+		if ( isset( $hook_extra['plugin'] ) ) {
+			$plugin      = get_plugin_data( $result['local_destination'] . '/' . $hook_extra['plugin'] );
+			$old_version = $this->current_version;
+			$new_version = $plugin['Version'];
 
-		if (
-			$response && // The response is not an error.
-			isset( $hook_extra['plugin'] ) && // We're running the right type of upgrade.
-			$this->name === $hook_extra['plugin'] && // We're updating the right thing.
-			! empty( $new_version ) && // The new version exists.
-			! empty( $old_version ) && // The old version exists.
-			$new_version !== $old_version // The new version is not the same as the old version.
-		) {
-			$this->run_migrations( $old_version, $new_version );
+			if (
+				$response && // The response is not an error.
+				$this->name === $hook_extra['plugin'] && // We're updating the right thing.
+				! empty( $new_version ) && // The new version exists.
+				! empty( $old_version ) && // The old version exists.
+				$new_version !== $old_version // The new version is not the same as the old version.
+			) {
+				$this->run_migrations( $old_version, $new_version );
+			}
+
+			$this->set_version( $new_version );
 		}
-
-		$this->set_version( $new_version );
-
 		return $response;
 	}
 }
