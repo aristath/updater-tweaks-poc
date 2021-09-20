72b070af2b13aee832095d8191ec9b57c0bd9d67
Ari Stathopoulos <aristath@gmail.com>
2021-09-20 09:48:28 +0300

    add wp_register_db_upgrade_route function

--- includes/db/db.php
+++ includes/db/db.php
@@ -5,6 +5,11 @@
  * @package WordPress
  */
 
+function wp_register_db_upgrade_route( $type, $name, $version, $routine_id, $callback ) {
+	$upgrader = new WP_Upgrader_DB( $type, $name );
+	$upgrader->register_routine( $version, $routine_id, $callback );
+}
+
 // Require the db-upgrader classes.
 require_once __DIR__ . '/class-wp-upgrader-db.php';
 
