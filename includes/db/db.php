9f785643bdd8d5c0b4204516f43a01e9a6caab62
Ari Stathopoulos <aristath@gmail.com>
2021-09-20 10:08:34 +0300

    inline docs

--- includes/db/db.php
+++ includes/db/db.php
@@ -5,6 +5,15 @@
  * @package WordPress
  */
 
+/**
+ * Register a database upgrade route.
+ *
+ * @param string   $type       Can be "plugin" or "theme".
+ * @param string   $name       The name of the plugin or theme.
+ * @param string   $version    The version of the plugin or theme.
+ * @param string   $routine_id The ID of the upgrade routine.
+ * @param callable $callback   The callback to run in order to upgeade the db.
+ */
 function wp_register_db_upgrade_route( $type, $name, $version, $routine_id, $callback ) {
 	$upgrader = new WP_Upgrader_DB( $type, $name );
 	$upgrader->register_routine( $version, $routine_id, $callback );
