c6546cfa05e82c72fdb1cca7443c0e6f37e493c6
Ari Stathopoulos <aristath@gmail.com>
2021-09-20 09:43:38 +0300

    files were deleted

--- includes/db/db.php
+++ includes/db/db.php
@@ -7,8 +7,6 @@
 
 // Require the db-upgrader classes.
 require_once __DIR__ . '/class-wp-upgrader-db.php';
-require_once __DIR__ . '/class-wp-upgrader-db-plugin.php';
-require_once __DIR__ . '/class-wp-upgrader-db-theme.php';
 
 // example implementation for a plugin.
 require_once __DIR__ . '/example-plugin-db-upgrader.php';
