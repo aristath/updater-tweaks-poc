d831fe5ebe9172c77970bce2c87da3ae4d5e94e5
Ari Stathopoulos <aristath@gmail.com>
2021-09-20 10:09:07 +0300

    use wp_register_db_upgrade_route

--- includes/db/example-plugin-db-upgrader.php
+++ includes/db/example-plugin-db-upgrader.php
@@ -1,4 +1,4 @@
-<?php
+<?php // phpcs:ignoreFile
 /**
  * This is an example implementation for a plugin database upgrading implementation.
  *
@@ -12,35 +12,44 @@
  * @return void
  */
 function my_plugin_upgrade_db() {
-	$upgrader = new WP_Upgrader_DB( 'plugin', 'wordpress-reset/wordpress-reset.php' );
-	$upgrader->register_routine(
-		'1.5', // Version.
-		'update_task_1', // Routine ID.
-		function() { error_log( 'Update task 1' ); }, // phpcs:ignore
+	wp_register_db_upgrade_route(
+		'plugin',                                     // The upgrade type. This is for a plugin.
+		'wordpress-reset/wordpress-reset.php',        // The plugin name.
+		'1.5',                                        // Version.
+		'update_task_1',                              // Routine ID.
+		function() { error_log( 'Update task 1' ); }, // The callback to run.
 	);
 
-	$upgrader->register_routine(
-		'1.0', // Version.
-		'update_task_2', // Routine ID.
-		function() { return new WP_Error( 'example_failed_upgrade', 'EXAMPLE FAIL' ); }, // phpcs:ignore
+	wp_register_db_upgrade_route(
+		'plugin',
+		'wordpress-reset/wordpress-reset.php',
+		'1.0',
+		'update_task_2',
+		function() { return new WP_Error( 'example_failed_upgrade', 'EXAMPLE FAIL' ); },
 	);
 
-	$upgrader->register_routine(
-		'1.1', // Version.
-		'update_task_3', // Routine ID.
-		function() { error_log( 'Update task 3' ); }, // phpcs:ignore
+	wp_register_db_upgrade_route(
+		'plugin',
+		'wordpress-reset/wordpress-reset.php',
+		'1.1',
+		'update_task_3',
+		function() { error_log( 'Update task 3' ); },
 	);
 
-	$upgrader->register_routine(
-		'1.1', // Version.
-		'update_task_4', // Routine ID.
-		function() { error_log( 'Update task 4' ); }, // phpcs:ignore
+	wp_register_db_upgrade_route(
+		'plugin',
+		'wordpress-reset/wordpress-reset.php',
+		'1.1',
+		'update_task_4',
+		function() { error_log( 'Update task 4' ); },
 	);
 
-	$upgrader->register_routine(
-		'0.3.2', // Version.
-		'update_task_5', // Routine ID.
-		function() { error_log( 'Update task 5' ); }, // phpcs:ignore
+	wp_register_db_upgrade_route(
+		'plugin',
+		'wordpress-reset/wordpress-reset.php',
+		'0.3.2',
+		'update_task_5',
+		function() { error_log( 'Update task 5' ); },
 	);
 }
 my_plugin_upgrade_db();
